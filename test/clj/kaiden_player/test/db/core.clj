be08bed51bf2171ac88c6493857d9842ff43bad0
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-08 18:28:29 +1000

    removed db folder

--- test/clj/kaiden_player/test/db/core.clj
+++
@@ -1,8 +0,0 @@
-(ns kaiden-player.test.db.core
-  (:require [kaiden-player.db.core :refer [*db*] :as db]
-            [luminus-migrations.core :as migrations]
-            [clojure.test :refer :all]
-            [clojure.java.jdbc :as jdbc]
-            [kaiden-player.config :refer [env]]
-            [mount.core :as mount]))
-
