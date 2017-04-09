ce851217974a8766d1f4b86c82b6b007de3d93a1
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-09 10:28:42 +1000

    returns unauthorized

--- src/clj/kaiden_player/routes/home.clj
+++ src/clj/kaiden_player/routes/home.clj
@@ -2,11 +2,12 @@
   (:require [kaiden-player.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.response :refer [response redirect content-type]]
-            [buddy.auth :refer [authenticated? throw-unauthorized]]))
+            [buddy.auth :refer [authenticated? throw-unauthorized]]
+            [ring.util.http-response :as response]))
 
 (defn home-page [request]
   (if-not (authenticated? request)
-    (redirect "/login")
+    (response/unauthorized)
     (layout/render "home.html")))
 
 (defroutes home-routes
