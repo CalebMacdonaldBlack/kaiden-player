2e78922cf82814419e21133f48d6ed2adb2f5cc4
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-17 19:48:12 +1000

    testing root template

--- test/cljs/kaiden_player/doo_runner.cljs
+++ test/cljs/kaiden_player/doo_runner.cljs
@@ -1,10 +1,13 @@
 (ns kaiden-player.doo-runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            [kaiden-player.core-test]))
+            [kaiden-player.core-test]
+            [kaiden-player.views.test-route-template]
+            [kaiden-player.test-handlers]
+            [kaiden-player.test-subscribers]))
 
 (doo-tests 'kaiden-player.core-test
+           'kaiden-player.views.test-route-template
            'kaiden-player.test-handlers
-           'kaiden-player.test-subscribers
-           'kaiden-player.views.test-route-template)
+           'kaiden-player.test-subscribers)
 
 
--- test/cljs/kaiden_player/views/test_route_template.cljs
+++ test/cljs/kaiden_player/views/test_route_template.cljs
@@ -2,18 +2,51 @@
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
             [pjstadig.humane-test-output]
             [reagent.core :as reagent :refer [atom]]
-            [kaiden-player.views.root-template :refer [nav-link]]
+            [kaiden-player.views.root-template :refer [nav-link pages navbar page]]
             [re-frame.core :as rf]))
+
+(defn- mock-page
+  []
+  [:h1 "test page"])
 
 (defn- mock-subscribe
   [coll]
   (case (first coll)
     :page (atom :home)))
 
+(defn- mock-subscribe2
+  [coll]
+  (case (first coll)
+    :page (atom :other)))
 
-(deftest test-nav-link
-  (testing "nav-link"
+(deftest test-root-template
+  (testing "nav-link with active link"
     (with-redefs [rf/subscribe mock-subscribe]
-      (let [expected [:a.item {:class "active", :href "#/"} "Home"]
-            actual (nav-link "#/" "Home" :home)]
-        (is (= actual expected))))))
+                 (let [expected [:a.item {:class "active", :href "#/"} "Home"]
+                       actual (nav-link "#/" "Home" :home)]
+                   (is (= actual expected)))))
+
+  (testing "nav-link without active link"
+    (with-redefs [rf/subscribe mock-subscribe2]
+                 (let [expected [:a.item {:class nil, :href "#/"} "Home"]
+                       actual (nav-link "#/" "Home" :home)]
+                   (is (= actual expected)))))
+
+  (testing "navbar"
+    (with-redefs [rf/subscribe mock-subscribe]
+                 (let [expected [:div.ui.menu
+                                 [:div.header.item "Kaiden Player"]
+                                 [:a.item {:class "active", :href "#/"} "Home"]
+                                 [:div.right.menu [:a.item {:href "/logout"} "Logout"]]]
+                       actual (navbar)]
+                   (is (= expected actual)))))
+  (testing "page"
+    (with-redefs [pages {:home #'mock-page}
+                  navbar [:nav]
+                  rf/subscribe mock-subscribe]
+                 (let [expected [:div
+                                 [[:nav]]
+                                 [#'kaiden-player.views.test-route-template/mock-page]]
+                       actual (page)]
+                   (is (= expected actual))))))
+
