f80d5235174df3595c3c9e4f5bf6f6d0380d4f0d
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-26 16:50:41 +1000

    subscriptions tests

--- test/cljs/kaiden_player/test_subscribers.cljs
+++ test/cljs/kaiden_player/test_subscribers.cljs
@@ -9,12 +9,21 @@
     (let [db {:page        :home
               :error-msg   "test err"
               :success-msg "test success"
-              :loading     true}
+              :loading     true
+              :current-song "song.mp3"
+              :dancing-gif "dancing-ninja.gif"
+              :music-playing true}
           page #'kaiden-player.subscriptions/page
           error-msg #'kaiden-player.subscriptions/error-msg
           success-msg #'kaiden-player.subscriptions/success-msg
-          loading #'kaiden-player.subscriptions/loading]
+          loading #'kaiden-player.subscriptions/loading
+          current-song #'kaiden-player.subscriptions/current-song
+          dancing-gif #'kaiden-player.subscriptions/dancing-gif
+          music-playing #'kaiden-player.subscriptions/music-playing]
       (is (= (page db) :home))
       (is (= (error-msg db) "test err"))
       (is (= (success-msg db) "test success"))
-      (is (loading db)))))
+      (is (loading db))
+      (is (= (current-song db) "song.mp3"))
+      (is (= (dancing-gif db) "dancing-ninja.gif"))
+      (is (music-playing db)))))
